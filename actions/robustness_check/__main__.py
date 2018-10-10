2852ae2f946028dbb6d5b8e7be6653a9e67d565c
Christian Kadner <ckadner@us.ibm.com>
2018-10-10 13:38:54 -0700

    Add robustness check action

---
+++ actions/robustness_check/__main__.py
@@ -0,0 +1,158 @@
+# OpenWhisk action to perform a model robustness check with ART on FfDL
+
+import json, os, requests, sys
+
+
+def run_safe(args):
+    try:
+        import boto3
+        import zipfile
+        from ruamel.yaml import YAML
+
+        yaml = YAML()
+
+        script_file = "robustness_check.py"
+        archive_file = "model.zip"
+        manifest_file = "manifest.yml"
+
+        def create_model_zip():
+            zipfile.ZipFile(archive_file, mode='w').write(script_file)
+
+        def create_cos_connection(params):
+            cos = boto3.resource("s3",
+                                 endpoint_url=params["aws_endpoint_url"],
+                                 aws_access_key_id=params["aws_access_key_id"],
+                                 aws_secret_access_key=params["aws_secret_access_key"])
+            return cos
+
+        def get_or_create_bucket(cos, bucket_name):
+            bucket = cos.Bucket(bucket_name)
+            if not bucket.creation_date:
+                bucket = cos.create_bucket(Bucket=bucket_name)
+            return bucket
+
+        def get_data_bucket_name(params):
+            # return "robustness-check-data" + params["model_id"][8:].lower()
+            return params["robustnesscheck_data_bucket"]
+
+        def get_result_bucket_name(params):
+            # return "robustness-check-results" + params["model_id"][8:].lower()
+            return params["robustnesscheck_results_bucket"]
+
+        def copy_file(source_bucket, source_file, target_bucket, target_file):
+            target_bucket.copy({"Bucket": source_bucket.name, "Key": source_file}, target_file)
+
+        def copy_training_result_files(cos, params):
+            model_id = params["model_id"]
+            tdb = get_or_create_bucket(cos, params["training_data_bucket"])
+            trb = get_or_create_bucket(cos, params["training_results_bucket"])
+            rcb = get_or_create_bucket(cos, get_data_bucket_name(params))
+            copy_file(tdb, params["dataset_file"], rcb, params["dataset_file"])
+            copy_file(trb, "%s/%s" % (model_id, params["networkdefinition_file"]), rcb, params["networkdefinition_file"])
+            copy_file(trb, "%s/%s" % (model_id, params["weights_file"]), rcb, params["weights_file"])
+
+        def create_manifest(params):
+            training_command = "\
+                pip3 install keras; \
+                pip3 install https://github.com/IBM/adversarial-robustness-toolbox/zipball/master; \
+                python3 robustness_check.py \
+                  --epsilon 0.2 \
+                  --data ${DATA_DIR}/%s \
+                  --networkdefinition ${DATA_DIR}/%s \
+                  --weights ${DATA_DIR}/%s"\
+                .replace("\
+                ", "") % (params["dataset_file"],
+                          params["networkdefinition_file"],
+                          params["weights_file"])
+
+            manifest_dict = {
+                "name": "art_robustness_check",
+                "description": "Generates adversarial samples to check model robustness using FGM",
+                "version": "1.0",
+                "memory": params.get("memory", "2Gb"),
+                "gpus": params.get("gpus", 0),
+                "cpus": params.get("cpus", 2),
+                "data_stores": [
+                    {
+                        "id": "robustness-check",
+                        "type": "mount_cos",
+                        "training_data": {
+                            "container": get_data_bucket_name(params)
+                        },
+                        "training_results": {
+                            "container": get_result_bucket_name(params)
+                        },
+                        "connection": {
+                            "auth_url":  params["aws_endpoint_url"],
+                            "user_name": params["aws_access_key_id"],
+                            "password":  params["aws_secret_access_key"]
+                        }
+                    }
+                ],
+                "framework": {
+                    "name": "tensorflow",
+                    "version": "1.5.0-py3",
+                    "command": training_command
+                },
+                "evaluation_metrics": {
+                    "type": "tensorboard",
+                    "in": "$JOB_STATE_DIR/logs/tb"
+                }
+            }
+            #yaml.dump(manifest_dict, open("manifest.yml", "w"), default_flow_style=False)
+            with open(manifest_file, "w") as f:
+                yaml.dump(manifest_dict, f)
+
+        def start_robustness_check(params):
+            url = "http://%s:%s/v1/models?version=2017-02-13" % (params["public_ip"], params["rest_api_port"])
+            headers = {"Accept": "application/json",
+                       "Authorization": params["basic_authtoken"],
+                       "X-Watson-Userinfo": params["watson_auth_token"]}
+            files = {'manifest': open('manifest.yml', 'rb'),
+                     'model_definition': open('model.zip', 'rb')}
+            response = requests.post(url, headers=headers, files=files)
+            return json.loads(response.text or response.content or "{}")
+
+        cos = create_cos_connection(args)
+        get_or_create_bucket(cos, get_data_bucket_name(args))
+        get_or_create_bucket(cos, get_result_bucket_name(args))
+        create_model_zip()
+        create_manifest(args)
+        copy_training_result_files(cos, args)
+        return start_robustness_check(args)
+    except Exception as e:
+        return {
+            "error": str(e)
+        }
+
+
+# main() method will be run when this action gets invoked
+# @param a JSON string or JSON file converted to a dict by OpenWhisk
+# @return The output of this action, which must be of type dict (converted to a JSON object by OpenWhisk)
+def main(parameters):
+    # run "safe" catching any exceptions and return errors as part of the result to help debugging
+    return run_safe(parameters)
+
+
+# --------------------------------
+# Helper methods for local testing
+# --------------------------------
+
+
+def _load_json_args():
+    import sys, json
+    arg1 = sys.argv[1] if len(sys.argv) > 1 else None
+    if arg1 and arg1.endswith(".json"):
+        with open(arg1) as json_file:
+            json_data = json.load(json_file)
+    elif arg1:
+        json_data = json.loads(arg1)
+    else:
+        raise ValueError("Expected JSON file or JSON string as argument.")
+    return json_data
+
+
+if __name__ == "__main__":
+    params = _load_json_args()
+    result = main(params)
+    print(result)
---
+++ actions/robustness_check/robustness_check.py
@@ -0,0 +1,138 @@
+import os
+import sys
+import numpy as np
+import numpy.linalg as la
+import json
+
+from keras.models import model_from_json
+from art.classifiers.keras import KerasClassifier
+from keras.utils import np_utils
+from art.attacks.fast_gradient import FastGradientMethod
+
+
+def get_metrics(model, x_original, x_adv_samples, y):
+    scores = model.evaluate(x_original, y, verbose=0)
+    model_accuracy_on_non_adversarial_samples = scores[1] * 100
+
+    y_pred = model.predict(x_original, verbose=0)
+    y_pred_adv = model.predict(x_adv_samples, verbose=0)
+
+    scores = model.evaluate(x_adv_samples, y, verbose=0)
+    model_accuracy_on_adversarial_samples = scores[1] * 100
+
+    pert_metric = get_perturbation_metric(x_original, x_adv_samples, y_pred, y_pred_adv, ord=2)
+    conf_metric = get_confidence_metric(y_pred, y_pred_adv)
+
+    data = {
+        "model accuracy on test data:": model_accuracy_on_non_adversarial_samples,
+        "model accuracy on adversarial samples": model_accuracy_on_adversarial_samples,
+        "reduction in confidence": conf_metric * 100,
+        "average perturbation": pert_metric * 100
+    }
+    return data
+
+
+def get_perturbation_metric(x_original, x_adv, y_pred, y_pred_adv, ord=2):
+    idxs = (np.argmax(y_pred_adv, axis=1) != np.argmax(y_pred, axis=1))
+
+    if np.sum(idxs) == 0.0:
+        return 0
+
+    perts_norm = la.norm((x_adv - x_original).reshape(x_original.shape[0], -1), ord, axis=1)
+    perts_norm = perts_norm[idxs]
+
+    return np.mean(perts_norm / la.norm(x_original[idxs].reshape(np.sum(idxs), -1), ord, axis=1))
+
+
+# This computes the change in confidence for all images in the test set
+def get_confidence_metric(y_pred, y_pred_adv):
+    y_classidx = np.argmax(y_pred, axis=1)
+    y_classconf = y_pred[np.arange(y_pred.shape[0]), y_classidx]
+
+    y_adv_classidx = np.argmax(y_pred_adv, axis=1)
+    y_adv_classconf = y_pred_adv[np.arange(y_pred_adv.shape[0]), y_adv_classidx]
+
+    idxs = (y_classidx == y_adv_classidx)
+
+    if np.sum(idxs) == 0.0:
+        return 0
+
+    idxnonzero = y_classconf != 0
+    idxs = idxs & idxnonzero
+
+    return np.mean((y_classconf[idxs] - y_adv_classconf[idxs]) / y_classconf[idxs])
+
+
+def main(argv):
+    if len(argv) < 2:
+        sys.exit("Not enough arguments provided.")
+
+    global network_definition_filename, weights_filename, dataset_filename
+
+    i = 1
+    while i <= 8:
+        arg = str(argv[i])
+        print(arg)
+        if arg == "--data":
+            dataset_filename = os.path.join(os.environ["DATA_DIR"], str(argv[i + 1]))
+        if arg == "--networkdefinition":
+            network_definition_filename = os.path.join(os.environ["DATA_DIR"], str(argv[i + 1]))
+        if arg == "--weights":
+            weights_filename = os.path.join(os.environ["DATA_DIR"], str(argv[i + 1]))
+        if arg == "--epsilon":
+            epsilon = float(argv[i + 1])
+
+        i += 2
+
+    print("dataset: ", dataset_filename)
+    print("network definition: ", network_definition_filename)
+    print("weights: ", weights_filename)
+
+    # load & compile model
+    json_file = open(network_definition_filename, 'r')
+    model_json = json_file.read()
+    json_file.close()
+    model = model_from_json(model_json)
+    model.load_weights(weights_filename)
+    comp_params = {'loss': 'categorical_crossentropy',
+                   'optimizer': 'adam',
+                   'metrics': ['accuracy']}
+    model.compile(**comp_params)
+
+    # create keras classifier
+    classifier = KerasClassifier((0, 1), model)
+
+    # load data set
+    pf = np.load(dataset_filename)
+
+    x = pf['x_test']
+    y = pf['y_test']
+
+    # pre-process numpy array
+
+    x = np.expand_dims(x, axis=3)
+    x = x.astype('float32') / 255
+
+    y = np_utils.to_categorical(y, 10)
+
+    # craft adversarial samples using FGSM
+    crafter = FastGradientMethod(classifier, eps=epsilon)
+    x_samples = crafter.generate(x)
+
+    # obtain all metrics (robustness score, perturbation metric, reduction in confidence)
+    metrics = get_metrics(model, x, x_samples, y)
+
+    print("metrics : ", metrics)
+
+    report_file = os.path.join(os.environ["RESULT_DIR"], "report.txt")
+
+    with open(report_file, "w") as report:
+        report.write(json.dumps(metrics))
+
+    adv_samples_file = os.path.join(os.environ["RESULT_DIR"], 'adv_samples')
+    print("adversarial samples saved to : ", adv_samples_file)
+    np.savez(adv_samples_file, x_original=x, x_adversarial=x_samples, y=y)
+
+
+if __name__ == "__main__":
+    main(sys.argv)
